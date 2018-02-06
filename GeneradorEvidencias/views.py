c7bb334ab242a11aee67ae94c0336026e0571ac1
Julián González <mtp91@intranet.local>
2018-02-06 10:51:39 +0100

    Modificamos plantillas y enlazamos con BBDD

--- GeneradorEvidencias/views.py
+++ GeneradorEvidencias/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Cliente
 
 def index(request):
     return HttpResponse("GENERADOR DE EVIDENCIAS")
 
 def form_generador_evidencias(request):
-    return render(request, 'GeneradorEvidencias/form_generador_evidencias.html', {})
+    clientes = Cliente.objects.filter(cliente="VODAFONE")
+    return render(request, 'GeneradorEvidencias/form_generador_evidencias.html', {'clientes': clientes})
