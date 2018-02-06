d98b49bd1a84707d8486f2c5a973306b789d707d
Julián González <mtp91@intranet.local>
2018-02-06 10:22:26 +0100

    Creamos vistas, plantillas

--- GeneradorEvidencias/views.py
+++ GeneradorEvidencias/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("GENERADOR DE EVIDENCIAS")
+
+def form_generador_evidencias(request):
+    return render(request, 'GeneradorEvidencias/form_generador_evidencias.html', {})
